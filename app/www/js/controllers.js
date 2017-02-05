8bc2a40303bee125d5cb481c8d5b686dfb12e7a5
Daniel Alves <alvesdesouza.daniel@gmail.com>
2017-02-05 01:57:38 -0200

    Implementação de CSS basico nas paginas login.html, profile.html, signup.html e tab-dash.html. Direcionamento da pagina login.html como index realizado na pagina app.js. Inclusão de uma pagina modal alocada no interior da pagina profile.html. Configuração de um simples checkbox localizado no modal profile/edição_profile para mostrar e esconder a senha. Obs.: Configuração realizada apena para teste da ferramenta

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -1,7 +1,7 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ionic'])
 
 .controller('DashCtrl', function($scope) {
-  var store = [
+  var infoDevice = [
     {id:1,order:3,type:"atuador",nameDevice:"Luz do Quarto",namePlace:"Quarto Casal",image:"img/lamp.png",statusConection:"img/success.png",status:"OFF"},
     {id:2,order:4,type:"atuador",nameDevice:"Luz da Sala",namePlace:"Sala",image:"img/abajur.png",statusConection:"img/success.png",status:"OFF"},
     {id:3,order:5,type:"atuador",nameDevice:"Luz Cozinha",namePlace:"Cozinha",image:"img/lamp.png",statusConection:"img/warning.png",status:"ON"},
@@ -9,7 +9,7 @@ angular.module('starter.controllers', [])
     {id:5,order:1,type:"atuador",nameDevice:"Cibele",namePlace:"Cozinha",image:"img/lamp.png",statusConection:"img/warning.png",status:"ON"},
     {id:6,order:6,type:"temperatura",nameDevice:"Luz do Lavabo",namePlace:"Lavabo",image:"img/therm.png",statusConection:"img/success.png",status:"28ºC"}
   ];
-  $scope.itens = store;
+  $scope.itens = infoDevice;
 
 })
 .controller('SignupCtrl', function($scope) {
@@ -20,7 +20,46 @@ angular.module('starter.controllers', [])
 
 
 })
-.controller('ProfileCtrl', function($scope) {
+.controller('ProfileCtrl', function($scope,$ionicModal) {
+  $scope.showPassword = function(checkButton) {
+    // console.log(checkButton);
+    if (checkButton == true) {
+      var x = document.getElementsByTagName("INPUT")[2];
+      x.getAttributeNode("type").value = "text";
+      var x = document.getElementsByTagName("INPUT")[3];
+      x.getAttributeNode("type").value = "text";
+    }else {
+      var x = document.getElementsByTagName("INPUT")[2];
+      x.getAttributeNode("type").value = "password";
+      var x = document.getElementsByTagName("INPUT")[3];
+      x.getAttributeNode("type").value = "password";
+    }
+  }
+
+  $ionicModal.fromTemplateUrl('editarPerfil', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  // Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
 
 
 })
